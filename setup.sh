31af40dfb062df9448c05de80f265446517bcf22
TheDataDominator <126510873+TheDataDominator@users.noreply.github.com>
2023-03-17 23:17:16 +0400

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Set the script name and directory
+SCRIPT_NAME="cxrebuster"
+SCRIPT_DIR="/usr/local/bin"
+
+# Create the script directory if it doesn't exist
+if [ ! -d "$SCRIPT_DIR" ]; then
+    echo "Creating directory $SCRIPT_DIR"
+    sudo mkdir -p "$SCRIPT_DIR"
+fi
+
+# Copy the script file to the script directory
+echo "Copying $SCRIPT_NAME to $SCRIPT_DIR"
+sudo cp "$SCRIPT_NAME" "$SCRIPT_DIR"
+
+# Set the correct permissions for the script file
+echo "Setting permissions for $SCRIPT_NAME"
+sudo chmod +x "$SCRIPT_DIR/$SCRIPT_NAME"
+
+# Print out the script location and usage information
+echo -e "The script has been installed to ${GREEN}$SCRIPT_DIR/$SCRIPT_NAME${NC}."
+echo "To run the script, use the following command:"
+echo -e "${GREEN}$SCRIPT_NAME <url> <wordlist>${NC}"
+
